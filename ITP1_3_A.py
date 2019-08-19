7f28f6b55716970d67bd14473dc4716aca4b8296
hiyoriaya <hiyoriaya@gmail.com>
2019-08-19 20:47:07 +0900

    ITP1_3_A clear

---
+++ ITP1_3_A.py
@@ -0,0 +1,11 @@
+# 複数の Hello World の出力
+#
+# 1000 個の "Hello World" を出力するプログラムを作成して下さい。
+#
+# Input
+# この問題に入力はありません。
+#
+# Output
+# 各行に "Hello World" と書かれた 1000 行の文字列を出力して下さい。
+for i in range(1000):
+    print("Hello World")
