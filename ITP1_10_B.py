7774573bddd7c3ae2a4e15145da25cd230795b21
hiyoriaya <hiyoriaya@gmail.com>
2019-09-11 22:48:20 +0900

    ITP1_10_B clear

--- ITP1_10_B.py
+++ ITP1_10_B.py
@@ -1,4 +1,18 @@
-a, b, c = map(float, input().split()) #4, 3, 90
-# height = c(sinb)
-# menseki = a * height / 2
-# d = (a**2)  + (b**2) - (2*(a*b*cos(c))
+# 三角形
+#
+# 三角形の２辺 a, b とその間の角 C から、その三角形の面積 S、周の長さ L, a を底辺としたときの高さ h を求めるプログラムを作成して下さい。
+#
+# Input
+# a の長さ, b の長さ, Cの大きさ（度）（整数）が空白区切りで与えられます。
+#
+# Output
+# S, L, h をそれぞれ1行に出力して下さい。0.0001以下の誤差があってもよいものとします。
+import math
+a, b, c = map(float, input().split())
+d = (a**2) + (b**2) - (2*(a*b * math.cos(math.radians(c))))
+L = a + b + (d**0.5)
+S = a * b * math.sin(math.radians(c)) / 2
+h = 2 * S / a
+print(str(S))
+print(str(L))
+print(str(h))
